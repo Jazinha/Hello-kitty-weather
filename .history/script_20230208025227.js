5208f0dcbcfb6cefe88cdccf8a3d9f7cc1c4636e
Jazmine Garcia <jazminegarcia2015@gmail.com>
2023-02-08 02:57:56 -0800

    added dropdown history

--- .history/script_20230208025227.js
+++ .history/script_20230208025227.js
@@ -1,24 +1,21 @@
 function addHistory(city) {
 
     var searchDisplay = document.getElementById("search-history");
-    const p = document.createElement("p");
-    const textNode = document.createTextNode(city);
+    // const a = document.createElement("a");
+    // const textNode = document.createTextNode(city);
    
-    p.appendChild(textNode);
+    // a.appendChild(textNode);
 
-    searchDisplay.appendChild(p);
+    // searchDisplay.appendChild(a);
 
+    // a.href = fetchWeather2(city);
 
-    // const btn = document.createElement("BUTTON");
-    // btn.innerHTML = city;
-   
-
-    // searchDisplay.appendChild(btn);
-
-    //  btn.addEventListener("click", () => {
-    //     fetchWeather2(city);
-    //     console.log("hi");
-    // })
+    const btn = document.createElement("BUTTON");
+    btn.innerHTML = city;
+    btn.addEventListener("click", () => {
+        fetchWeather2(city);
+    })
+    searchDisplay.appendChild(btn);
     
 
     var oldCities = localStorage.getItem("cities");
@@ -114,69 +111,69 @@ function fetchWeather(inputTxt)
 }
 
 
-// function fetchWeather2(inputTxt)
-// {
-//     var APIKEY = '3d90c1ba80e1377a6db30bdf0b2f1488';
-//     var searchBtn = document.getElementById("search-btn");
-//     searchBtn.addEventListener("click", () => {
-//         var inptTextValue = inputTxt.value;
-//         fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${inptTextValue}&appid=${APIKEY}`)
-//             .then((response) => response.json())
-//             .then((data) => {
-//                 var weathers = data.list;
-//                 var eachDays = Array();
-//                 for (var i = 0; i < weathers.length; i += 8) {
-//                     // console.log(weathers[i]);
-//                     eachDays.push({
-//                         condition: weathers[i].weather[0].main,
-//                         temperature: weathers[i].main.temp,
-//                         humidity: weathers[i].main.humidity,
-//                         windSpeed: weathers[i].wind.speed,
-//                         icon: weathers[i].weather[0].icon,
-//                         date: weathers[i].dt_txt
-//                     });
+function fetchWeather2(inputTxt)
+{
+    var APIKEY = '3d90c1ba80e1377a6db30bdf0b2f1488';
+    var searchBtn = document.getElementById("search-btn");
+    searchBtn.addEventListener("click", () => {
+        var inptTextValue = inputTxt.value;
+        fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${inptTextValue}&appid=${APIKEY}`)
+            .then((response) => response.json())
+            .then((data) => {
+                var weathers = data.list;
+                var eachDays = Array();
+                for (var i = 0; i < weathers.length; i += 8) {
+                    // console.log(weathers[i]);
+                    eachDays.push({
+                        condition: weathers[i].weather[0].main,
+                        temperature: weathers[i].main.temp,
+                        humidity: weathers[i].main.humidity,
+                        windSpeed: weathers[i].wind.speed,
+                        icon: weathers[i].weather[0].icon,
+                        date: weathers[i].dt_txt
+                    });
 
-//                     var conditionDisplay = ``;
-//                 }
+                    var conditionDisplay = ``;
+                }
                 
-//                 // console.log(eachDays);
+                // console.log(eachDays);
 
-//                 var currentDayCity = document.getElementById("city");
-//                 var currentIcon = document.getElementById("icon");
-//                 var currentCondition = document.getElementById("condition");
-//                 var currentTemperature = document.getElementById("temperature");
-//                 var currentHumidity = document.getElementById("humidity");
-//                 var currentWindSpeed = document.getElementById("wind-speed");
+                var currentDayCity = document.getElementById("city");
+                var currentIcon = document.getElementById("icon");
+                var currentCondition = document.getElementById("condition");
+                var currentTemperature = document.getElementById("temperature");
+                var currentHumidity = document.getElementById("humidity");
+                var currentWindSpeed = document.getElementById("wind-speed");
                 
-//                 currentDayCity.innerText = inptTextValue + " (" + eachDays[0].date + ") ";
-//                 currentIcon.src = `http://openweathermap.org/img/wn/${eachDays[0].icon}@2x.png`;
+                currentDayCity.innerText = inptTextValue + " (" + eachDays[0].date + ") ";
+                currentIcon.src = `http://openweathermap.org/img/wn/${eachDays[0].icon}@2x.png`;
 
-//                 currentTemperature.innerHTML = eachDays[0].temperature;
-//                 currentHumidity.innerHTML = eachDays[0].humidity;
-//                 currentCondition.innerHTML = eachDays[0].condition;
-//                 currentWindSpeed.innerHTML = eachDays[0].windSpeed;
+                currentTemperature.innerHTML = eachDays[0].temperature;
+                currentHumidity.innerHTML = eachDays[0].humidity;
+                currentCondition.innerHTML = eachDays[0].condition;
+                currentWindSpeed.innerHTML = eachDays[0].windSpeed;
                
                
-//                 for (var i = 1; i < 6; i+=1)
-//                 {
-//                     var icon = document.getElementById(`day-${i}-icon`);
-//                     var day = document.getElementById(`day-${i}-date`);
-//                     var condition = document.getElementById(`day-${i}-conditions`);
-//                     var day_temp = document.getElementById(`day-${i}-temp`);
-//                     var humidity = document.getElementById(`day-${i}-humidity`);
-//                     var windSpeed = document.getElementById(`day-${i}-wind`);
+                for (var i = 1; i < 6; i+=1)
+                {
+                    var icon = document.getElementById(`day-${i}-icon`);
+                    var day = document.getElementById(`day-${i}-date`);
+                    var condition = document.getElementById(`day-${i}-conditions`);
+                    var day_temp = document.getElementById(`day-${i}-temp`);
+                    var humidity = document.getElementById(`day-${i}-humidity`);
+                    var windSpeed = document.getElementById(`day-${i}-wind`);
                     
-//                     icon.src = `http://openweathermap.org/img/wn/${eachDays[i-1].icon}.png`;
-//                     day.innerHTML = " (" + eachDays[i-1].date + ") ";
-//                     condition.innerHTML = eachDays[i-1].condition;
-//                     day_temp.innerHTML = eachDays[i-1].temperature;
-//                     humidity.innerHTML = eachDays[i-1].humidity;
-//                     windSpeed.innerHTML = eachDays[i-1].windSpeed;
-//                 }
+                    icon.src = `http://openweathermap.org/img/wn/${eachDays[i-1].icon}.png`;
+                    day.innerHTML = " (" + eachDays[i-1].date + ") ";
+                    condition.innerHTML = eachDays[i-1].condition;
+                    day_temp.innerHTML = eachDays[i-1].temperature;
+                    humidity.innerHTML = eachDays[i-1].humidity;
+                    windSpeed.innerHTML = eachDays[i-1].windSpeed;
+                }
 
-//             });
-//         })
-// }
+            });
+        })
+}
 
 
 function init() {
